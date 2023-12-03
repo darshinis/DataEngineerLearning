2cdda64d02dc2f62449dcc677031852a3831d8dd
darshinis <52904264+darshinis@users.noreply.github.com>
2023-12-03 20:58:43 +0530

    Update initialProcessing.py

--- learning1/initialProcessing.py
+++ learning1/initialProcessing.py
@@ -1,1 +1,22 @@
+##############################################################################################
+"""
+Step :
+1. create sparSession : spark = SparkSession.builder.master("local[*]").getOrCreate() - entry point for spark applications
+2. create sparkContext : sc = spark.sparkContext - used to create RDD from values and files
+"""
+##############################################################################################
+#                                       step 1                                               #
+##############################################################################################
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+sc = spark.sparkContext
+##############################################################################################
+#                        Step : reading csv from git - commad : !wget                        #
+##############################################################################################
+df = spark.read.csv("cars.csv",header = True,sep=";")
+df = spark.read.format("csv")\ # type of file
+                .option("header"=True)\
+                .option("delimiter"=";)\
+                .option("mode"="PERMISSIVE")\ # (mode : PERMISSIVE,DROPMALFORMED,FAILFAST)
+                .load("cars.csv")
+##############################################################################################
 
